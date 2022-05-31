cdc3b335fb72a274f521dd410623af823c05ac8b
lauty-shock <lautaroperez135@gmail.com>
2022-05-31 18:50:59 -0300

    bubbleSort terminado

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -7,6 +7,8 @@ function factorear(num) {
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
 
+
+
 }
 
 function bubbleSort(array) {
@@ -14,6 +16,20 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+
+  let swap = true;
+  while(swap){
+    swap = false;
+    for(let i=0;i<array.length-1;i++){
+      if(array[i]>array[i+1]){
+        let aux = array[i];
+        array[i] = array[i+1];
+        array[i+1] = aux;
+        swap = true;
+      }
+    }
+  }
+  return array;
 
 }
 
