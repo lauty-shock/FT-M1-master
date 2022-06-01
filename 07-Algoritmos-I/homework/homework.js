e4558607dad824bbfed11d444ffd7b67df2b5e70
lauty-shock <lautaroperez135@gmail.com>
2022-05-31 23:46:38 -0300

    Actualizaciones

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,9 +6,17 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
-
-
+  let array=[1]; //1,2,2,3,3,5
+  let divisor=2; //3,4,5
+  while(num!==1){
+    if(num%divisor === 0){
+      array.push(divisor)
+      num=num/divisor;
+    }else{
+      divisor++
+    }
+  }
+  return array;
 }
 
 function bubbleSort(array) {
@@ -16,21 +24,19 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
-  let swap = true;
-  while(swap){
-    swap = false;
-    for(let i=0;i<array.length-1;i++){
-      if(array[i]>array[i+1]){
-        let aux = array[i];
-        array[i] = array[i+1];
-        array[i+1] = aux;
-        swap = true;
+  let swap = true; //Declaro una variable que le permita el paso
+  while(swap){ //Pregunto si el paso esta permitido
+    swap = false;//Declaro el pase en falso (como si hubiese gastado una entrada)
+    for(let i=0;i<array.length-1;i++){ //Recorro el array hasta la penultima posicion, porque la ultima posicion es para intercambiar
+      if(array[i]>array[i+1]){ //Si el valor de la posicion actual es mayor que su siguiente posicion:
+        let aux = array[i]; //Guardo el valor actual en una variable "aux"
+        array[i] = array[i+1]; //Cambio el valor de la posicion actual por el valor de la posicion siguiente
+        array[i+1] = aux; //Cambio el valor de la siguiente posicion por el valor actual que había guardado en "aux"
+        swap = true; //Le doy un pase para que vuelva a pasar el while
       }
     }
   }
-  return array;
-
+  return array; //Retorno el array listo
 }
 
 
